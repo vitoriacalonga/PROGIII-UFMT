c4a48da3f3c840eb86d93b5a2747a2d37ddba491
vitoriacalonga <133722930+vitoriacalonga@users.noreply.github.com>
2025-10-13 10:59:58 -0300

    Commit inicial com as atividades de aula

---
+++ atividades_aula/fibonacci.java
@@ -0,0 +1,27 @@
+//Programa que imprime os 30 primeiros elementos da sequência de Fibonacci
+
+package atividades_aula;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 30;
+        long s1 = 1;
+        long s2 = 1;
+        
+        System.out.println("Os 30 primeiros elementos da sequencia de Fibonacci: ");
+        for(int i = 1 ; i <=n; i++){
+            if(i == 1 || i == 2){
+                System.out.print("1");
+            }
+            else{
+                long proximo = s1 + s2;
+                System.out.print(proximo);
+                s1 = s2;
+                s2 = proximo;
+            }
+            if(i < n){
+                System.out.print(", ");
+            }
+        }
+    }
+}
